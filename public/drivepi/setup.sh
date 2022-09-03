2d9794610c0438a1b01edd25b0bc5a7b52418881
Jacobtread <jacobtread@gmail.com>
2022-09-03 19:34:37 +1200

    Updated scripts

---
+++ public/drivepi/setup.sh
@@ -0,0 +1,33 @@
+# SETUP SCRIPT FOR DRIVE-PI
+# SOURCED: https://strepo.jacobtread.com/drivepi/setup.sh
+
+# Update the system repositories and run upgrades
+apt-get update && apt-get upgrade -y
+
+# systemd-resolve conflicts with dnsmasq so it must be disabled
+systemctl disable systemd-resolved
+systemctl stop systemd-resolved
+unlink /etc/resolv.conf
+echo nameserver 8.8.8.8 | tee /etc/resolv.conf
+
+# Install network manager, samba and dnsmasq
+apt-get install -y network-manager samba dnsmasq
+
+# Restart dnsmasq
+systemctl restart dnsmasq
+
+# Allow samba through the firewall
+sudo ufw allow samba
+
+# Move to bin directory
+cd /bin || exit
+mkdir drivepi
+cd drivepi || exit
+
+# Download drivepi server
+curl -o server https://strepo.jacobtread.com/drivepi/server
+# Replace the local samba config with the drivepi config
+curl -o /etc/samba/smb.conf https://strepo.jacobtread.com/drivepi/smb.conf
+
+# Execute permission on server
+chmod +x server
